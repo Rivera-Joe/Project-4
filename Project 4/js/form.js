872c95b532f0da6921dd81a022fedd384411567b
Rivera-Joe <36744600+Rivera-Joe@users.noreply.github.com>
2019-10-30 18:46:13 -0500

    update
    added form csss and pattern for form.
    
    also changed form submission to 'onsubmit' rather than when submit button is pressed.

--- Project 4/js/form.js
+++ Project 4/js/form.js
@@ -2,26 +2,16 @@ var display = document.querySelector('#display');
 
 var user = {};
 
-
 function saveInfo(){
-    
- 
-        user.cabin = document.querySelector('#cabin').options[document.querySelector('#cabin').selectedIndex].value;
-        user.fName = document.querySelector('#fName').value;
-        user.lName = document.querySelector('#lName').value;
-        user.phoneNumber = document.querySelector('#phoneNumber').value;
-        user.address = document.querySelector('#address1').value;
-        user.city = document.querySelector('#city').value;
-        user.state = document.querySelector('#state').options[document.querySelector('#state').selectedIndex].value;
-        user.bookDate = document.querySelector('#bookDate').value;
-
-        var userConfirm = confirm(`Please verify the information below is correct:\nCabin: ${user.cabin}\nBooking Date: ${user.bookDate}\nFirst Name: ${user.fName}\nLast Name: ${user.lName}\nPhone Number: ${user.phoneNumber}\nAddress: ${user.address}\nCity: ${user.city}\nState: ${user.state}`);
-
-        if(userConfirm == true){
-            alert(`Booking for ${user.fName} ${user.lName} on ${user.bookDate} created`);
-
-        }
+user.cabin = document.querySelector('#cabin').options[document.querySelector('#cabin').selectedIndex].value;
+user.fName = document.querySelector('#fName').value;
+user.lName = document.querySelector('#lName').value;
+user.phoneNumber = document.querySelector('#phoneNumber').value;
+user.address = document.querySelector('#address1').value;
+user.city = document.querySelector('#city').value;
+user.state = document.querySelector('#state').options[document.querySelector('#state').selectedIndex].value;
 
 
+alert(`${user.cabin}\n${user.fName}\n${user.lName}\n${user.phoneNumber}\n${user.address}\n${user.city}\n${user.state}`);
 
 }
